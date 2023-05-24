978d1f1100eb61329abd27d822eb77148aee1220
madeyexz <ian.xiao@stu.pku.edu.cn>
2023-05-24 19:23:14 +0800

    ok

--- demo_2.py
+++ demo_2.py
@@ -1,4 +1,5 @@
 from langchain.llms import OpenAI
+from langchain.chat_models import ChatOpenAI
 from langchain.chains import ConversationChain
 from langchain.memory import ConversationBufferMemory
 from langchain.prompts.prompt import PromptTemplate
@@ -6,7 +7,7 @@ import timeit # lib used to count time
 from langchain.callbacks import get_openai_callback # lib used to count tokens
 
 
-def config(llm = OpenAI(temperature=0.5), verbose_opt=False, template="Hello! No template is set:{history}{input}"):
+def config(llm = ChatOpenAI(model_name="gpt-3.5-turbo-0301",temperature=0.4), verbose_opt=False, template="Hello! No template is set:{history}{input}"):
     '''configures the AI model, conversation chain, memory and prompt'''
     # llm = OpenAI(temperature=0.5)
     
@@ -72,20 +73,26 @@ def save_conversation(conver_data, callback):
 def main():
     interviewer_template = ''' 
     You are an interviewer. You are interviewing a candidate for a job. You and the candidate will be given a business case.
-    The goal of the interview is to solve the business case together with the candidate, but you will only be giving necessary hints to the candidate (according to the given case information), and you would not make anything up.
+    The goal of the interview is to solve the business case together with the candidate, but you will not give relevent information unless the candidate asks about it, and you would not make anything up.
 
-    The given case is: "A major chemical manufacturer produces a chemical product used to preserve foods in containers. Despite an increase in market share, the manufacturer has experienced a decline in profits. The CEO of the company is worried about this trend and hires you to investigate."
+    The information for the candidate is: "You are the Dean of a large public university in the Midwest.
+    Your tenure has focused on raising the profile of the university through various initiatives. Recently, the Board of Regents has requested a plan that outlines the university’s environmental footprint and a goal to reach net-zero waste by 2040. As a former business executive, you are familiar with the principles of ESG, but need to understand what the major drivers of environmental impact at PubU are."
     Additional information is given here:
     "
-    - The company experienced no significant increase in cost in the last couple of years related to any additional fixed or variable cost.
-    - On the revenue side, the company's output increased slightly, slightly higher than the industry average
-    - Competition has decreased. A number of players have exited the industry because they were losing money.
-    - The companies which left felt that the industry had gotten saturated.
-    - Sales for the industry overall have decreased in the last few years, indicating a general negative trend and less demand for the product.
-    - Substitute products are not really being used. This is because preservatives, in general, are being used less in foods, and many consumers now prefer fresh food as their top choice.
-    - The makers of food are experiencing decreased volume, as the entire industry has been slowing down.
-    - They are forced to lower their prices to survive, and in order to lower costs, they are also using their leverage to renegotiate price structures of raw materials.
-    - Costs have stayed the same.
+    - PubU is a large research institution consisting of an undergrad (35k students), graduate school (10k) and various professional schools (~2k). Number in student body is expected to be stable. The university is very large and requires an extensive diesel-powered bus system for students to commute all over campus.
+    - The average operating budget of PubU is $2B. Of that $2B, roughly 30% comes from the state, 10% from alumni donations, 20% from the university endowment (fund that pays out money each year) and 40% from student fees. 
+    - PubU currently gets electricity from the local grid (which uses a mix of coal, gas, wind, and solar). The local utility is transitioning to 100% carbon-free by 2030. 
+    - Net-zero waste means consuming only as much energy as produced, achieving a sustainable balance between water availability and demand, and eliminating solid waste sent to landfills.
+    - PubU’s goali is to reach net-zero by 2040
+    - 1 bus per route, 2 miles per gallon, each route runs on average, four times an hour, for 15 hours a day, seven days a week, 50 weeks a year.
+    - $6 / gallon of diesel. Other maintenance costs include $20,000 per bus in annual maintenance
+    - 20 lbs CO2 / gallon of diesel
+    - Total Annual Cost of Diesel: Round 104 miles to 100 miles
+    - Cost per bus: $1M 
+    - Annual Maintenance Cost: $4,000 / bus
+    - Bus Efficiency: 4 miles / gallon of diesel equivalent (gallon of diesel equivalent is a measure of energy)
+    - Electric to Diesel Conversion: 25 gallon of diesel equivalent / megawatt-hour (MW-h) 
+    - Grid Stats: $100 / MW-h generated, 1000 lbs CO2 / MW-h generated (currently, future is 0)-
     "
     Current converstaion:
     {history}
